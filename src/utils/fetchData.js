231826b4d98cd5d3f895a1650beab3e4b1d4a7b1
Priyanshu Gupta <priyanshu2859@gmail.com>
2023-09-08 17:38:21 +0530

    hiding-api-key

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions= {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '10c09878e4msh622511cc7bf0ef7p1cd256jsn00055ed8fbf7',//process.env.REACT_APP_RAPID_API_KEY, 
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY, 
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
@@ -9,7 +9,7 @@ export const exerciseOptions= {
 export const youtubeOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'a31c37a35cmsh813aa312bd7f0cbp1bbf6cjsne6eaa16a165c',
+      'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEY,
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
